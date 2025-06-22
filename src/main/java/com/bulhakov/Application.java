619f86874fc8371396842dfd0f25cfd8b794c1b7
anthony.bulhakov <anthony.bulgakov@gmail.com>
2025-06-22 15:12:26 +0300

    Added images. Redirected STDERR stream to STDOUT

--- src/main/java/com/bulhakov/Application.java
+++ src/main/java/com/bulhakov/Application.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+        // Solution to display logs on Railway
+        System.setErr(System.out);
         SpringApplication.run(Application.class, args);
     }
 }
