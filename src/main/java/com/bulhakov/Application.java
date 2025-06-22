60093c4e14875c2ebf4824ee1339a5195e8c27a8
anthony.bulhakov <anthony.bulgakov@gmail.com>
2025-06-22 15:30:03 +0300

    Removed STDERR to STDOUT redirect

--- src/main/java/com/bulhakov/Application.java
+++ src/main/java/com/bulhakov/Application.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        // Solution to display logs on Railway
-        System.setErr(System.out);
         SpringApplication.run(Application.class, args);
     }
 }
